a7b1cb6cae77eaa9b3e05768d98a14cf6188200f
marroccuandrea <marroccuandrea@yahoo.it>
2024-05-10 16:39:20 +0200

    fix style header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <h1>Header</h1>
+    <h1 class="text-center">Films</h1>
 
     <a class="mx-4" href="{{ route('home') }}">Home</a>
     <a class="mx-4" href="{{ route('movies-title') }}">Film in ordine di titolo</a>
